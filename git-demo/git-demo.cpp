fd2c1cbfb980e5fdf76ddb8994fced5b07b21926
d-gnecco <work.dgnecco@gmail.com>
2024-11-02 13:02:02 -0700

    feat: basic input string parser Added logic to parse an input string with the format "<num1><op><num2>" Stores the parsed data in repsective variables and exits with an error if the format isn't followed

--- git-demo/git-demo.cpp
+++ git-demo/git-demo.cpp
@@ -1,8 +1,60 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+	cout << "Input format: \"<num1><op><num2>\", where <op> is +, -, *, or /" << endl;
 
+	string input_str;
+	cin >> input_str;
+
+	double num1, num2;
+	int op_pos = 0;
+
+	// parse op
+	for (int i = 0; i < input_str.length(); i++)
+	{
+		if (!isdigit(input_str[i]) && input_str[i] != '.')
+		{
+			op_pos = i;
+			break;
+		}
+	}
+	char op = input_str[op_pos];
+
+	// parse nums
+	try {
+		num1 = stod(input_str.substr(0, op_pos));
+		num2 = stod(input_str.substr(op_pos + 1));
+	}
+	catch(exception e) {
+		cout << "\nError: Invalid input" << endl;
+		exit(1);
+	}
+
+	// execute appropriate calc function
+	switch (op)
+	{
+		case '+':
+
+			break;
+
+		case '-':
+
+			break;
+
+		case '*':
+
+			break;
+
+		case '/':
+
+			break;
+
+		default:
+			cout << "\nError: Invalid operator" << endl;
+			exit(1);
+	}
 }
